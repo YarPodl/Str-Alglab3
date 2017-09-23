5278b3ff319325e58ac28e2fde31211656385daf
YarPodl <yar.podlesnov@yandex.ru>
2017-09-23 17:20:19 +0300

    Наследование хешей

---
+++ src/HashSearch/Hash.java
@@ -0,0 +1,72 @@
+package HashSearch;
+
+
+
+public abstract class Hash {
+
+    protected static final double multiplier = 0.6180339887;
+    protected static final int divisor = 11817;
+
+    abstract int getHash(long value, int countAddress);
+
+}
+public class HashMethodDividing extends Hash{
+    int getHash(long value, int countAddress) {
+        return (int) ((value % divisor) * ((double)countAddress / divisor));
+    }
+}
+    public class hashMethodMidSquares extends Hash{
+        int getHash(long value, int countAddress) {
+            return
+        }
+    }
+public class hashMethodCurtailing extends Hash{
+    int getHash(long value, int countAddress) {
+        return
+    }}
+public class hashMethodMethodMultipl extends Hash{
+    int getHash(long value, int countAddress) {
+        return
+    }}
+    static int methodDividing(long value, int countAddress){
+        return (int) ((value % divisor) * ((double)countAddress / divisor));
+    }
+
+    static int methofMidSquares(long value, int countAddress){
+        int lengthAddress = Integer.toString(countAddress).length(); // порядок числа адресов
+        long valueSqr = value*value;
+        int lengthValueSqr = Long.toString(valueSqr).length();
+        while (lengthValueSqr > lengthAddress) {
+            if ((lengthValueSqr - lengthAddress) % 2 == 0){
+                valueSqr = valueSqr / 10;
+                valueSqr =  (long)(valueSqr % Math.pow(10, lengthValueSqr - 1));
+                lengthValueSqr -= 2;
+            }
+            if ((lengthValueSqr - lengthAddress) % 2 == 1){
+                valueSqr = valueSqr / 10;
+                lengthValueSqr -= 1;
+            }
+        }
+        return (int) (valueSqr * ((double)countAddress /  Math.pow(10, lengthAddress)));
+    }
+
+    static int methodCurtailing(long value, int countAddress){
+        int lengthAddress = Integer.toString(countAddress).length(); // порядок числа адресов
+        int lengthValueSqr = Long.toString(value).length();
+        int result = 0;
+        while (value != 0){
+            result += value % Math.pow(10, lengthValueSqr + 1);
+            value /= Math.pow(10, lengthValueSqr);
+            if (Integer.toString(result).length() > lengthAddress){
+                result /= 10;
+            }
+        }
+        return result;
+    }
+
+    static  int methodMultipl (long value, int countAddress){
+        double i = value * multiplier;
+        i = i - (int) i;    // Дробная часть числа
+        return (int) (countAddress * i);
+    }
+}
---
+++ src/HashSearch/HashSearch.java
@@ -0,0 +1,120 @@
+package HashSearch;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class HashSearch {
+
+
+    private final int maxNumber = 65000;
+    private long listKey[];
+
+
+
+    long hashListOpenAddress[];
+    ArrayList<Long>[] hashListChain;
+
+
+    public void
+
+
+    public void newListKey(int count){
+        listKey = new long[count];
+        Random random = new Random();
+        for (int i = 0; i < count; i++) {
+            listKey[i] = random.nextInt(maxNumber);
+        }
+    }
+
+    HashSearch(int count){
+        newListKey(count);
+        /*
+        hashListMethodDividing = new long[count];
+        hashListMethodMidSquares = new long[count];
+        hashListMethodCurtailing = new long[count];
+        hashListMethodMultipl = new long[count];
+
+        hashArrayListMethodDividing = new ArrayList<Long>[];
+        hashArrayListMethodMidSquares[];
+        hashArrayListMethodCurtailing[];
+        hashArrayListMethodMultipl[];
+
+        infill();
+        */
+    }
+
+
+
+    // Добавляет одно хэш значение в массив методом открытой адресации
+
+    private void addMethodOpenAddress(long[] list, int index, long value) {
+        if (list[index] != 0){
+            int j = index + 1;
+            while (list[index] != 0){
+                j++;
+                if (j == list.length) {j = 0;}
+            }
+        }
+        list[index] = value;
+    }
+
+
+    // Заполняет хэш значениями массив методом цепочек
+
+    private void infillMethodChain(){
+        hashListChain[]
+    }
+
+
+
+    // Заполняет хэш значениями массивы методом открытой адресации
+
+    private void infill(){
+        int hashMethodDividing;
+        int hashMethodMidSquares;
+        int hashMethodCurtailing;
+        int hashMethodMethodMultipl;
+        for (int i = 0; i < list.length; i++) {
+            /*
+            hashMethodDividing = Hash.methodDividing(list[i], list.length, divisor);
+            hashMethodMidSquares = Hash.methodMidSquares(list[i], list.length);
+            hashMethodCurtailing = Hash.methodCurtailing(list[i], list.length);
+            hashMethodMethodMultipl = Hash.methodMultipl(list[i], list.length, multiplier);
+
+            addOpenAddress(hashListMethodDividing, hashMethodDividing, list[i]);
+            addOpenAddress(hashListMethodMidSquares, hashMethodMidSquares, list[i]);
+            addOpenAddress(hashListMethodCurtailing, hashMethodCurtailing, list[i]);
+            addOpenAddress(hashListMethodMultipl, hashMethodMethodMultipl, list[i]);
+            */
+        }
+    }
+
+
+    /*
+    long hashListMethodDividing[];
+    long hashListMethodMidSquares[];
+    long hashListMethodCurtailing[];
+    long hashListMethodMultipl[];
+
+    ArrayList<Long> hashArrayListMethodDividing[]; // Тут изменить!!
+    ArrayList<Long> hashArrayListMethodMidSquares[];
+    ArrayList<Long> hashArrayListMethodCurtailing[];
+    ArrayList<Long> hashArrayListMethodMultipl[];
+    */
+
+    /*
+    private void hashOpenAddressMethodDividing(){
+
+    }
+    private void hashOpenAddressMethodMidSquares(){
+
+    }
+    private void hashOpenAddressMethodCurtailing(){
+
+    }
+    private void hashOpenAddressMethodMultipl(){
+
+    }
+
+    */
+}
