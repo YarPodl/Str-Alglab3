85e5549cacd95ebede77c944ee1444a4b10d8742
YarPodl <yar.podlesnov@yandex.ru>
2017-09-24 14:38:44 +0300

    Form 2

--- src/Main/Form.java
+++ src/Main/Form.java
@@ -4,5 +4,9 @@ import javax.swing.*;
 
 public class Form {
     private JSpinner spinner1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JTextField textField4;
     private JTextField textField1;
+    private JButton посчитатьButton;
 }
